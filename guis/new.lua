681d98c728c65832846fedf934bfb032b509cf78
yanrewrite <trulystarize@gmail.com>
2026-06-11 11:48:42 +0700

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -323,7 +323,7 @@ local function downloadFile(path, func)
 	if not isfile(path) then
 		createDownloader(path)
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/MaxlaserTech/CatV6/'..readfile('catrewrite/profiles/commit.txt')..'/'..select(1, path:gsub('catrewrite/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/yanrewrite/meowvape/'..readfile('catrewrite/profiles/commit.txt')..'/'..select(1, path:gsub('catrewrite/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
